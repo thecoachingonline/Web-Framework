4ab8cbbac716a5e4d51497026715018b72cb42fb
Y.Nanthachai <thecoachingonline@gmail.com>
2025-04-15 12:45:26 +0000

    .NET v9 Blazor

--- Blazor/MyApp/Program.cs
+++ Blazor/MyApp/Program.cs
@@ -18,10 +18,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
