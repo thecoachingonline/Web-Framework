e8531e908b0ad6ffb17de7db0e2e846af3a7b938
Nanthachai Yuennan <thecoachingonline@gmail.com>
2025-06-08 02:42:34 +0000

    Angular v20

--- Angular/myapp/README.md
+++ Angular/myapp/README.md
@@ -1,6 +1,6 @@
 # Myapp
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.7.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.1.
 
 ## Development server
 
--- Angular/myapp/src/app/app.ts
+++ Angular/myapp/src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
-export class AppComponent {
-  title = 'myapp';
+export class App {
+  protected title = 'myapp';
 }
